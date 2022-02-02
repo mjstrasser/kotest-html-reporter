a7eddb452a70df1a8c07c061853cd513a9a6c559
Michael Strasser <michael@michaelstrasser.com>
2022-02-02 22:36:29 +1000

    Signing and publishing snapshot to Sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,18 +16,16 @@
 
 */
 
-import mjs.kotest.building.configureAssemble
-import mjs.kotest.building.configurePublishing
-import mjs.kotest.building.configureSpotless
-import mjs.kotest.building.configureTesting
-import mjs.kotest.building.configureVersioning
-import mjs.kotest.building.configureWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.dokka") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
     `java-library`
+    signing
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "com.michaelstrasser"
@@ -55,11 +53,69 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
-val ktlintVersion: String by project
+tasks.dokkaHtml.configure {
+    outputDirectory.set(buildDir.resolve("dokka"))
+}
 
-configureAssemble()
-configurePublishing()
-configureSpotless(ktlintVersion)
-configureTesting()
-configureVersioning()
-configureWrapper()
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+tasks.register<Jar>("dokkaJar") {
+    archiveClassifier.set("javadoc")
+    from(layout.buildDirectory.dir("dokka/html"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("kotlinLibrary") {
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["dokkaJar"])
+            pom {
+                name.set("kotes-html-reporter")
+                description.set("Kotest plugin to create HTML reports of test runs")
+                url.set("https://github.com/mjstrasser/kotest-html-reporter")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                    developers {
+                        developer {
+                            id.set("mjstrasser")
+                            name.set("Michael Strasser")
+                            email.set("kotest-html-reporter@michaelstrasser.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/mjstrasser/kotest-html-reporter.git")
+                        url.set("https://github.com/mjstrasser/kotest-html-reporter")
+                    }
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    val ossrhUsername: String? by project
+    val ossrhPassword: String? by project
+    repositories {
+        create("sonatype") {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(ossrhUsername)
+            password.set(ossrhPassword)
+        }
+    }
+}
+
+signing {
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications["kotlinLibrary"])
+}
