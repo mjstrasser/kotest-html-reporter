1456ab7389f3e9772d394aa12a59f849eb01ecc4
Michael Strasser <michael@michaelstrasser.com>
2022-02-05 14:07:32 +1000

    Markdown conversion improvements

--- src/main/kotlin/mjs/kotest/Markdown.kt
+++ src/main/kotlin/mjs/kotest/Markdown.kt
@@ -1,10 +1,11 @@
 package mjs.kotest
 
+private val paraRegex = Regex("\n *\n")
 private val codeRegex = Regex("`([^`]+)`")
 private val italicRegex = Regex("_(\\S[^_]+)_")
 private val boldRegex = Regex("\\*\\*(\\S[^_]+)\\*\\*")
 
-private fun String.para() = this.replace("\n\n", "<br><br>")
+private fun String.para() = paraRegex.replace(this, "<br><br>")
 private fun String.code() = codeRegex.replace(this, "<span class='code'>\$1</span>")
 private fun String.italic() = italicRegex.replace(this, "<em>\$1</em>")
 private fun String.bold() = boldRegex.replace(this, "<strong>\$1</strong>")
--- src/test/kotlin/mjs/kotest/MarkdownTest.kt
+++ src/test/kotlin/mjs/kotest/MarkdownTest.kt
@@ -5,30 +5,44 @@ import io.kotest.matchers.shouldBe
 
 class MarkdownTest : FunSpec({
 
-    description("""Extension property that converts inline Markdown to `HTML`.
-        |
-        |This description contains _actual_ examples.
-        |
-        |**Yes** it really does.
-    """.trimMargin())
+    description(
+        """
+        | This is an example of using the `description` extension property, that
+        | can be used to describe the code being tested by a spec. 
+        | 
+        | You can put limited Markdown in the description and it will be converted to HTML.
+        | 
+        | - It converts `code in backticks`.
+        | 
+        | - It converts **bold** text.
+        | 
+        | - It converts _italic_ text.
+        | 
+        | (It does not convert lists.)
+    """.trimMargin()
+    )
 
-    test("converts double newlines into double line breaks") {
+    test("it converts double newlines into double line breaks") {
         "one\n\ntwo".convertMarkdown() shouldBe "one<br><br>two"
     }
 
-    test("converts code at the start of strings") {
+    test("it ignores blank lines when converting double newlines") {
+        "one\n     \ntwo".convertMarkdown() shouldBe "one<br><br>two"
+    }
+
+    test("it converts code at the start of strings") {
         "`one` and two".convertMarkdown() shouldBe "<span class='code'>one</span> and two"
     }
-    test("converts code in the middle of strings") {
+    test("it converts code in the middle of strings") {
         "one `and` two".convertMarkdown() shouldBe "one <span class='code'>and</span> two"
     }
-    test("converts code at the end of strings") {
+    test("it converts code at the end of strings") {
         "one and `two`".convertMarkdown() shouldBe "one and <span class='code'>two</span>"
     }
-    test("converts multi-word code") {
+    test("it converts multi-word code") {
         "one `and two`".convertMarkdown() shouldBe "one <span class='code'>and two</span>"
     }
-    test("converts multi-line code") {
+    test("it converts multi-line code") {
         "one `and\n\ntwo`".convertMarkdown() shouldBe "one <span class='code'>and<br><br>two</span>"
     }
 
