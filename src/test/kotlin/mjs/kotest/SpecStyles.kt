ab73242ff7342cb5bbe8d118493ee263007efdd8
Michael Strasser <michael@michaelstrasser.com>
2022-07-15 23:41:05 +1000

    Tests using all Kotest styles

---
+++ src/test/kotlin/mjs/kotest/SpecStyles.kt
@@ -0,0 +1,95 @@
+package mjs.kotest
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.core.spec.style.FeatureSpec
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.WordSpec
+
+fun randomTestResult() = listOf(
+    ::passingTest,
+    ::failingTest,
+    ::errorTest,
+).random().invoke()
+
+class FunSpecExample : FunSpec({
+    test("Using `FunSpec`") {
+        randomTestResult()
+    }
+})
+
+class DescribeSpecExample : DescribeSpec({
+    describe("Using `DescribeSpec`") {
+        it("with `it` nested in `describe`") {
+            randomTestResult()
+        }
+    }
+})
+
+class ShouldSpecExample : ShouldSpec({
+    should("Using `ShouldSpec`") {
+        randomTestResult()
+    }
+})
+
+class StringSpecExample : StringSpec({
+    "Using `StringSpec`" {
+        randomTestResult()
+    }
+})
+
+class BehaviourSpecExample : BehaviorSpec({
+    Given("Using `BehaviourSpec`") {
+        When("the test runs") {
+            Then("it passes or not") {
+                randomTestResult()
+            }
+        }
+    }
+})
+
+class FreeSpecExample : FreeSpec({
+    "Using `FreeSpec`" - {
+        "can nest" - {
+            "another level" {
+                randomTestResult()
+            }
+        }
+    }
+})
+
+class WordSpecExample : WordSpec({
+    "Using `WordSpec`" should {
+        "pass or not" {
+            randomTestResult()
+        }
+    }
+})
+
+class FeatureSpecExample : FeatureSpec({
+    feature("Using `FeatureSpec`") {
+        scenario("with a scenario") {
+            randomTestResult()
+        }
+    }
+})
+
+class ExpectSpecExample : ExpectSpec({
+    context("Using `ExpectSpec`") {
+        expect("pass or not") {
+            randomTestResult()
+        }
+    }
+})
+
+class AnnotationSpecExample : AnnotationSpec() {
+    @Test
+    fun `Using AnnotationSpec`() {
+        randomTestResult()
+    }
+}
